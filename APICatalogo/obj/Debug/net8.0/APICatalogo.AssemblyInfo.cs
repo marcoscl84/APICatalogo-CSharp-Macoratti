ce835214ec47d38eb93f640d7a683cf66d30428c
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2024-03-03 10:57:26 -0300

    migrations

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8398ba7f320df11d6072a8ec7f1ef5fb7b13c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e295994c6a7d7003814dc9f40d8c4a0932723c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
