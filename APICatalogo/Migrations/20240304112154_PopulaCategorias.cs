aec81eb4b81938a83adf37e7c4c78385df22f83f
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2024-03-04 08:32:24 -0300

    populando tabela categorias

---
+++ APICatalogo/Migrations/20240304112154_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into categoria (Nome, ImagemUrl) values ('Bebida', 'bebidas.jpg')");
+            mb.Sql("insert into categoria (Nome, ImagemUrl) values ('Lanches', 'lanches.jpg')");
+            mb.Sql("insert into categoria (Nome, ImagemUrl) values ('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from categoria");
+        }
+    }
+}
--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce835214ec47d38eb93f640d7a683cf66d30428c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ef0697671ec55d4f04862eb37e517fa61940a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
