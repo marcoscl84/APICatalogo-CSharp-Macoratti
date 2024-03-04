0de9d23894318521319ee01551ceb527541fa7db
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2024-03-04 15:47:07 -0300

    populando tabela produtos

---
+++ APICatalogo/Migrations/20240304183901_PopulaProdutos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos (Nome,Descricao,Preco,ImageUrl,Estoque,DataCadastro,CategoriaId) values ('Coke','Refri de Cola 350ml',5.45,'coke.jpg',50,now(),1)");
+            mb.Sql("Insert into Produtos (Nome,Descricao,Preco,ImageUrl,Estoque,DataCadastro,CategoriaId) values ('Sanduba','Sanduba de atum',8.90,'sand.jpg',50,now(),1)");
+            mb.Sql("Insert into Produtos (Nome,Descricao,Preco,ImageUrl,Estoque,DataCadastro,CategoriaId) values ('Pudim','Pudim de leite',4.30,'pudim.jpg',50,now(),1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ef0697671ec55d4f04862eb37e517fa61940a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec81eb4b81938a83adf37e7c4c78385df22f83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
